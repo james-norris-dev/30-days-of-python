0df237ab996ead5c23b71c4bf32ec47a92b76004
James Norris <james.norris.developer@gmail.com>
2025-12-12 00:35:07 -0500

    created a new directory, day-1, added helloworld.py, worked w/ operations, string, print() and type() functions

---
+++ day-1/helloworld.py
@@ -0,0 +1,38 @@
+""" Day 1 of 30 Days of Python
+ Exercises - Day 1
+ Exercise: Level 2"""
+
+# Using different operations on the operands 3 and 4
+print(4 + 3)
+print(4 - 3)
+print(3 - 4)
+print(4 * 3)
+print(4 % 3)
+print(3 % 4)
+print(4 / 3)
+print(3 / 4)
+print(4 ** 3)
+print(3 ** 4)
+print(4 // 3)
+print(3 // 4)
+
+print('')
+# Writing strings to the python interactive shell
+print('Jame')
+print('Norris')
+print('USA')
+print('I am enjoying 30 days of python')
+
+print('')
+# Checking the type - with function 'type()'
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Your name'))
+print(type('Your family name'))
+print(type('Your country'))
+
+
+
