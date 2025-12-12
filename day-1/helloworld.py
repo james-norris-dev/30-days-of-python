10caa55d902293a774077188cd77ca98dad79c7c
James Norris <james.norris.developer@gmail.com>
2025-12-12 01:05:04 -0500

    added a list of different data types

--- day-1/helloworld.py
+++ day-1/helloworld.py
@@ -34,5 +34,33 @@ print(type('Your name'))
 print(type('Your family name'))
 print(type('Your country'))
 
+print('')
+print(f"12, Integer (int)")
+print(type(12))
+print(f"12.3456, Float (float)")
+print(type(12.3456))
+print(f"My name, String (str)")
+print(type("My name"))
+print(f"True, Boolean (bool)")
+print(type(True))
+# Lists - [], a mutable ordered collection, can hold multiple data types
+example_list = [12, 12.3456, 'My name', True]
+print(f"{example_list}, List (list)")
+print(type(example_list))
+# Tuples - (), an immutable ordered collection, can hold multiple data types
+example_tuple = (12, 12.3456, 'My name', True)
+print(f"{example_tuple}, Tuple (tuple)")
+print(type(example_tuple))
+# Set - {}, a mutable unordered collection - no duplicates
+example_set = {12, 12.3456, 'My name', True}
+print(f"{example_set}, Set (set)")
+print(type(example_set))
+# Dictionary - {}, an immutable ordered collection of key_value pairs - keys are case sensitive
+example_dict = {'integer': 12,
+                'float': 12.3456,
+                'string': 'My name',
+                'boolean': True}
+print(f"{example_dict}, Dict (dict)")
+print(type(example_dict))
 
 
