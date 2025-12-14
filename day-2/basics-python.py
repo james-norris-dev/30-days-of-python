8af5d4642c171caac3acd46f0122178eec73a18d
James <justjamescreative@outlook.com>
2025-12-13 20:10:38 -0500

    Worked on python basics, built-in functions, and started variables

---
+++ day-2/basics-python.py
@@ -0,0 +1,8 @@
+# comment
+print('The above statement is a comment.')
+# to get an output from Python you need to use the print() function
+print('Lets see how print works')
+print('first', 'second', 'whatever', 2021, 250, True, False, ['Mango', 'Papaya', 'Avocado'])
+# python has many builtin functions - examples: print(), len(), input(), round(), etc.
+print('I love the print function')
+print('Hello, world!')
